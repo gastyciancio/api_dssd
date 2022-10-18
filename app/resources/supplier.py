b616cb9114502a38f5a444bce75fe896f329259e
Gaston Ciancio <gastonciancio4@gmail.com>
2022-10-17 21:34:00 -0300

    agregue metadata de materiales sin proveedores

--- app/resources/supplier.py
+++ app/resources/supplier.py
@@ -22,4 +22,19 @@ def by_data():
 
     suppliers = Supplier.get_suppliers(materiales, filtro_precio,dias_extra)
     suppliers = [ supplier.json() for supplier in suppliers ]
-    return jsonify({'suppliers': suppliers })
+
+    # Retorno en un arreglo los materiales que no tengan un proveedor asi tenemos registro de los mismos
+    materiales_sin_supplier = []
+    for material in materiales:
+        tiene_supplier= False
+        for supplier in suppliers:
+            for material_of_supplier in supplier['materials']:
+                if (tiene_supplier == True):
+                    break
+                if (material_of_supplier['name'].lower() == material['name'].lower()):
+                    tiene_supplier = True
+                    break
+        if (tiene_supplier == False):
+            materiales_sin_supplier.append(material)
+
+    return jsonify({'suppliers': suppliers, 'metadata':{'materiales_sin_proveedor': materiales_sin_supplier} })
